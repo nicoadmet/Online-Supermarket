0953bfd797c4e9682d179506d25616ab5a278c52
Nicolás Admet <nicolasadmet@gmail.com>
2025-06-25 21:27:40 -0300

    Preparo deploy para GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Online-Supermarket', 
+  assetPrefix: '/Online-Supermarket', 
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
